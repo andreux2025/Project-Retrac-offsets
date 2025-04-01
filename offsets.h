f9a3cbc5a024e9feb45b77e1dec4e300ee376da9
andreux2025 <turandreu124@gmail.com>
2025-04-01 08:13:23 -0700

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,17 @@
+bHasStartedFloating;                               // 0x1434
+struct FVector                                RelativeScale3D;                                   // 0x0134
+class UAnimMontage*                           EquipAnimation;                                    // 0x0B00
+class UCharacterMovementComponent*            CharacterMovement;                                 // 0x0288
+EMovementMode                                 MovementMode;                                      // 0x0168
+struct FVector                                PendingLaunchVelocity;                             // 0x03C4
+float                                         ViewYawMax;                                        // 0x2790(0x0004)(Edit, BlueprintVisible, ZeroConstructor, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
+float                                         ViewRollMin;                                       // 0x2794(0x0004)(Edit, BlueprintVisible, ZeroConstructor, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
+TWeakObjectPtr<class AActor>                  CurrentVehicle;                                    // 0x0300
+float                                         EmoteToFireCooldownTime;                           // 0x0974
+float                                         ChargeTime;                                        // 0x0968
+bool                                          bIgnoreTryToFireSlotCooldownRestriction;           // 0x0C79
+class UFortWeaponItemDefinition*              WeaponData;                                        // 0x0378
+float                                         ReviveFromDBNOTime;                                // 0x2DF8
+float                                         DBNOStartTime;                                     // 0x2DFC
+float                                         DBNOInvulnerableTime;                              // 0x2E00
+bool                                          bADSWhileNotOnGround;                              // 0x3331
