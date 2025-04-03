c80090d66e91a56253522cbe2e1ddad40fc648ca
andreux2025 <turandreu124@gmail.com>
2025-04-03 14:26:20 -0700

    Update getbone.h

--- getbone.h
+++ getbone.h
@@ -7,3 +7,4 @@ Vector3 GetEntityBone(uintptr_t mesh, int bone_id)
 	FTransform ComponentToWorld = read<FTransform>(mesh + 0x1C0);
 	D3DMATRIX matrix = MatrixMultiplication(bone.ToMatrixWithScale(), ComponentToWorld.ToMatrixWithScale());
 	return Vector3(matrix._41, matrix._42, matrix._43);
+}
